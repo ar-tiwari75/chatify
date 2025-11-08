9a589d354a4d9046c063f4efa7be79632216d76c
Adarsh Tiwari <adarshrtiwari9@gmail.com>
2025-11-09 03:13:53 +0530

    signup controller added

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,14 +1,22 @@
 import express from "express"
 import dotenv from "dotenv"
+import connectDB from "./lib/db.js"
+import authRoutes from "./routes/auth.route.js"
+
 dotenv.config();
 const app = express();
 
 const PORT = process.env.PORT || 5000;
 
+app.use(express.json()); // Middleware to parse JSON bodies -> req.body
+
 app.get('/', (req, res) => {
     res.send('Hello World!');
 })
 
+app.use('/api/auth', authRoutes)
+
 app.listen(PORT, () => {
     console.log('Server is running on port ' + PORT);
+    connectDB();
 })
