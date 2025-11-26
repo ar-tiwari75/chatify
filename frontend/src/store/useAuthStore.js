66831c62314ac879a70f6c7bfd2c98c9aea34350
Adarsh Tiwari <adarshrtiwari9@gmail.com>
2025-11-27 00:11:52 +0530

    Frontend Login Page implemented

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -5,6 +5,7 @@ export const useAuthStore = create((set) => ({
     authUser: null,
     isCheckingAuth: true,
     isSigningUp: false,
+    isLoggingIn: false,
 
     checkAuth: async () => {
         try {
@@ -28,6 +29,19 @@ export const useAuthStore = create((set) => ({
             toast.error(error.response.data.message);
         } finally {
             set({isSigningUp: false})
+        }
+    },
+
+    login: async(data) => {
+        set({isLoggingIn: true})
+        try {
+            const res = await axiosInstance.post('/auth/login', data);
+            set({ authUser: res.data });
+            toast.success('Logged in successfully!');
+        } catch (error) {
+            toast.error(error.response.data.message);
+        } finally {
+            set({isLoggingIn: false})
         }
     }
 }));
